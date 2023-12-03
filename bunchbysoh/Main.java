d4473a0c43adaad7adea20fb152582adb1e06334
KChiragN <114282433+KChiragN@users.noreply.github.com>
2023-12-03 17:42:35 +0530

    Update1 Main.java
    Updated code

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -35,6 +35,27 @@ public class Main {
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+    int[] zeroSoHCapacities = {0, 0, 0}; //when batteries have 0% SoH
+    CountsBySoH zeroSoHCounts = countBatteriesByHealth(zeroSoHCapacities);
+    assert(zeroSoHCounts.healthy == 0);
+    assert(zeroSoHCounts.exchange == 0);
+    assert(zeroSoHCounts.failed == 3);
+    int[] edgeCaseCapacities = {96, 96, 96, 96}; // 80% of 120Ah capacity
+    CountsBySoH edgeCaseCounts = countBatteriesByHealth(edgeCaseCapacities);
+    assert(edgeCaseCounts.healthy == 3);
+    assert(edgeCaseCounts.exchange == 0);
+    assert(edgeCaseCounts.failed == 0);
+    int[] edgeCaseCapacities = {74, 74, 74}; // 62% of 120Ah capacity
+    CountsBySoH edgeCaseCounts = countBatteriesByHealth(edgeCaseCapacities);
+    assert(edgeCaseCounts.healthy == 0);
+    assert(edgeCaseCounts.exchange == 3);
+    assert(edgeCaseCounts.failed == 0);
+    int[] emptyArray = {}; //when input array is empty
+    CountsBySoH emptyCounts = countBatteriesByHealth(emptyArray);
+    assert(emptyCounts.healthy == 0);
+    assert(emptyCounts.exchange == 0);
+    assert(emptyCounts.failed == 0);
+    assert(presentCapacities.length == counts.healthy + counts.exchange + counts.failed);//Total count of batteries is equal to no of batteries in the array
     System.out.println("Done counting :)\n");
   }
 
